18324e693a3d63c0ebdea4d204925dd7cdcbfc71
Daylen Jones <jonesday001@warren.k12.in.us>
2024-09-26 12:38:37 -0400

    update

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public class Vehicle {
+public class main {
+
+    private String make;
+    private String model;
+
+
+    public main(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+
+    public String getMake() {
+        return make;
+    }
+
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+
+    public String getModel() {
+        return model;
+    }
+
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
+public class main{
+
+    public static void main(String[] args) {
+
+        Vehicle myCar = new Vehicle("Ford", "Fusion");
+
+
+        System.out.println("Make: " + myCar.getMake());
+        System.out.println("Model: " + myCar.getModel());
+
+
+        myCar.setMake("Chevy");
+        myCar.setModel("Malibu");
+
+
+        System.out.println("Updated Make: " + myCar.getMake());
+        System.out.println("Updated Model: " + myCar.getModel());
+    }
 }
